93b266c16f4ac497d064e6d8c4dd9322b7cb1825
Lars Götte <lars.goette@drive.eu>
2016-04-05 10:59:38 +0200

    POWER-99 Links auf Detailspage

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -101,7 +101,7 @@ function errorHandler (error) {
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services', 'pascalprecht.translate'])
+angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services', 'pascalprecht.translate', 'ngIOS9UIWebViewPatch'])
 
     .directive('browseTo', function ($ionicGesture) {
         return {
